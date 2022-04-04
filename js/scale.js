47023f9e3150993c8c5cb13eb57edba8d0f99718
Tuiaara87 <77272499+Tuiaara87@users.noreply.github.com>
2022-04-04 16:00:54 +0300

    Помощь другу

--- js/scale.js
+++ js/scale.js
@@ -2,8 +2,6 @@ const scaleControlValue = document.querySelector('.scale__control--value');
 const scaleControlSmaller = document.querySelector('.scale__control--smaller');
 const scaleControlBigger = document.querySelector('.scale__control--bigger');
 const imgPreview = document.querySelector('.img-upload__preview');
-// const effectsRadioInput = document.querySelector('.effects__radio');
-// const imgUploadPreview = document.querySelector('.img-upload__preview');
 const filterOriginal = document.querySelector('.effects__preview--none');
 const filterChrome = document.querySelector('.effects__preview--chrome');
 const filterSepia = document.querySelector('.effects__preview--sepia');
@@ -11,7 +9,15 @@ const filterMarvin = document.querySelector('.effects__preview--marvin');
 const filterPhobos = document.querySelector('.effects__preview--phobos');
 const filterHeat = document.querySelector('.effects__preview--heat');
 const sliderValue = document.querySelector('.effect-level__slider');
-const currentFilter = 'none';
+let currentFilter = 'none';
+let currentScaleValue = 100;
+const FilterNames = {
+  CHROME: 'chrome',
+  SEPIA: 'sepia',
+  MARVIN: 'marvin',
+  PHOBOS: 'phobos',
+  HEAT: 'heat',
+};
 
 noUiSlider.create(sliderValue, {
   range: {
@@ -35,34 +41,41 @@ noUiSlider.create(sliderValue, {
 
 sliderValue.noUiSlider.on('update', (_, handle, unencoded) => {
   const value = unencoded[handle];
-  if (currentFilter === 'chrome') {
-    imgPreview.style.filter = `grayscale(${value})`;
+  if (currentFilter === FilterNames.CHROME) {
+    imgPreview.style.filter = `grayscale(${value / 100})`;
   }
-  if (currentFilter === 'sepia') {
+  if (currentFilter === FilterNames.SEPIA) {
     imgPreview.style.filter = `sepia(${value})`;
   }
-  if (currentFilter === 'marvin') {
+  if (currentFilter === FilterNames.MARVIN) {
     imgPreview.style.filter = `invert(${value}%)`;
   }
-  if (currentFilter === 'phobos') {
+  if (currentFilter === FilterNames.PHOBOS) {
     imgPreview.style.filter = `blur(${value}px)`;
   }
-  if (currentFilter === 'heat') {
+  if (currentFilter === FilterNames.HEAT) {
     imgPreview.style.filter = `brightness(${value})`;
   }
 });
 
 scaleControlSmaller.addEventListener('click', () => { // Значение должно изменяться с шагом в 25.
-  scaleControlValue.value -= 25;
-  const scaleValue = scaleControlValue.value / 100;
-  imgPreview.style.transform = `scale(${scaleValue})`; // если в поле стоит значение 75%, то в стиле изображения должно быть написано transform: scale(0.75)
+  if (currentScaleValue === 25) {
+    return;
+  }
+  currentScaleValue -= 25;
+  scaleControlValue.value = `${currentScaleValue}%`;
+  imgPreview.style.transform = `scale(${currentScaleValue / 100})`; // если в поле стоит значение 75%, то в стиле изображения должно быть написано transform: scale(0.75)
 });
 
 scaleControlBigger.addEventListener('click', () => {
-  scaleControlValue.value = +scaleControlValue.value + 25;
-  const scaleValue = scaleControlValue.value / 100;
-  imgPreview.style.transform = `scale(${scaleValue})`;
-});
+  if (currentScaleValue === 100) {
+    return;
+  }
+  currentScaleValue += 25;
+  scaleControlValue.value = `${currentScaleValue}%`;
+  imgPreview.style.transform = `scale(${currentScaleValue / 100})`;
+}
+);
 
 filterOriginal.addEventListener('click', () => {
   imgPreview.className = 'img-upload__preview';
@@ -72,24 +85,29 @@ filterOriginal.addEventListener('click', () => {
 filterChrome.addEventListener('click', () => {
   imgPreview.className = 'img-upload__preview';
   imgPreview.classList.add('effects__preview--chrome');
+  currentFilter = FilterNames.CHROME;
 });
 
 filterSepia.addEventListener('click', () => {
   imgPreview.className = 'img-upload__preview';
   imgPreview.classList.add('effects__preview--sepia');
+  currentFilter = FilterNames.SEPIA;
 });
 
 filterMarvin.addEventListener('click', () => {
   imgPreview.className = 'img-upload__preview';
   imgPreview.classList.add('effects__preview--marvin');
+  currentFilter = FilterNames.MARVIN;
 });
 
 filterPhobos.addEventListener('click', () => {
   imgPreview.className = 'img-upload__preview';
   imgPreview.classList.add('effects__preview--fobos');
+  currentFilter = FilterNames.PHOBOS;
 });
 
 filterHeat.addEventListener('click', () => {
   imgPreview.className = 'img-upload__preview';
   imgPreview.classList.add('effects__preview--heat');
+  currentFilter = FilterNames.HEAT;
 });
